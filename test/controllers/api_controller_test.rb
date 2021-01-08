dfdbca96d28197b0b4f7026c8f9246acc57599d5
denalibalser <denali.balser167@gmail.com>
2021-01-08 11:20:03 -0700

    set up backend file structure, added Postt controller, model, migration, and routes

---
+++ test/controllers/api_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ApiControllerTest < ActionDispatch::IntegrationTest
+  test "should get v1" do
+    get api_v1_url
+    assert_response :success
+  end
+
+  test "should get Posts" do
+    get api_Posts_url
+    assert_response :success
+  end
+
+end
